aa8c2532ea3aa5616abb0f702ba33407e3d678ca
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2026-07-01 17:20:44 -0500

    Add HTTPS server configuration for Swagger UI

--- src/main/java/com/rodrigomv/edutrackbackend/config/OpenApiConfig.java
+++ src/main/java/com/rodrigomv/edutrackbackend/config/OpenApiConfig.java
@@ -5,8 +5,11 @@ import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -14,6 +17,9 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .servers(List.of(
+                    new Server().url("https://edutrack.solidwebs.com").description("Production")
+                ))
                 .info(new Info()
                         .title("EduTrack API")
                         .description("Documentación de la API REST de EduTrack")
