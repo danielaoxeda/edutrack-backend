5d54a0a94d36309b50aa6f41e9093025a7db56b2
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2026-07-01 16:43:03 -0500

    Fix CORS configuration for edutrack.solidwebs.com

--- src/main/java/com/rodrigomv/edutrackbackend/config/SecurityConfig.java
+++ src/main/java/com/rodrigomv/edutrackbackend/config/SecurityConfig.java
@@ -59,10 +59,11 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*"));
+        configuration.setAllowedOrigins(List.of("https://edutrack.solidwebs.com"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setExposedHeaders(List.of("Authorization"));
+        configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
