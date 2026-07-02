80969070082f6d3df3fb41a85040c8032c891e5b
rodrigomaldonadov <rodrigomaldonadov8@gmail.com>
2026-07-01 20:55:38 -0500

    fix: update UserDetailsService reference in JwtAuthenticationFilter and mark CustomUserDetailsService as primary

--- src/main/java/com/rodrigomv/edutrackbackend/security/CustomUserDetailsService.java
+++ src/main/java/com/rodrigomv/edutrackbackend/security/CustomUserDetailsService.java
@@ -12,12 +12,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Primary;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Primary
 @RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
